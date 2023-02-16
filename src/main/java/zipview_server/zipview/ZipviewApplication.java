7b720f8bd09c259314b83b9539975ec7962387b9
reed426 <red426@sookmyung.ac.kr>
2023-02-16 10:01:39 +0900

    zipview init

---
+++ src/main/java/zipview_server/zipview/ZipviewApplication.java
@@ -0,0 +1,13 @@
+package zipview_server.zipview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zipview_server/zipview/ZipviewApplicationTests.java
@@ -0,0 +1,13 @@
+package zipview_server.zipview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
