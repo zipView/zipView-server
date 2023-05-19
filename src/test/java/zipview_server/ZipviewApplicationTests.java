44bdb2c7f9130dcf79326e55cb843ee46612e361
Kiltae <loverpleasestayo0@gmail.com>
2023-05-19 19:56:43 +0900

    [Test] 리뷰 DB 저장

---
+++ src/test/java/zipview_server/ZipviewApplicationTests.java
@@ -0,0 +1,18 @@
+package zipview_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest(classes = ZipviewApplication.class)
+@Transactional
+class ZipviewApplicationTests{
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
+
+
