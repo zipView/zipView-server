588a8964ed39c478ca535772306974d52c30798f
Kiltae <loverpleasestayo0@gmail.com>
2023-05-20 14:06:47 +0900

    [FEAT] 코멘트 도메인

---
+++ src/main/java/zipview_server/domain/Comment.java
@@ -0,0 +1,13 @@
+package zipview_server.domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Comment extends BaseEntity {
+    @Id @GeneratedValue
+    private Long id;
+
+    // User !!
+
+
+}
