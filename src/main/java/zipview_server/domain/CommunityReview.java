0e950c9d0b897aeae81eeed3483321bf6faf67ee
Kiltae <loverpleasestayo0@gmail.com>
2023-05-23 15:36:19 +0900

    [CHORE] 게시글 이미지 업로드 관련

---
+++ src/main/java/zipview_server/domain/CommunityReview.java
@@ -0,0 +1,19 @@
+/*package zipview_server.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class CommunityReview extends Review{
+  /*  @Builder
+    public CommunityReview(int price, String content, String title, int likeNum, RoomType roomType, Residence residence) {
+        super(price, content, title, likeNum, roomType, residence);
+
+    }
+}
+*/
---
+++ src/main/java/zipview_server/repository/CommunityReviewRepository.java
@@ -0,0 +1,8 @@
+/*package zipview_server.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import zipview_server.domain.CommunityReview;
+
+public interface CommunityReviewRepository extends JpaRepository<CommunityReview, Long> {
+}
+*/
