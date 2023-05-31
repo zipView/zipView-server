a029e7189c4816aae0cead51e6e2ded193b5a900
Kiltae <loverpleasestayo0@gmail.com>
2023-05-31 17:56:01 +0900

    [FEAT] 리뷰 좋아요 도메인 생성

---
+++ src/main/java/zipview_server/domain/LikeReview.java
@@ -0,0 +1,42 @@
+package zipview_server.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LikeReview extends BaseEntity{
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    @Column(nullable = false)
+    private boolean status;
+
+    //user 연결
+
+    public static LikeReview createLikeReview(Review review) {
+        LikeReview likeReview = new LikeReview();
+        likeReview.review = review;
+        likeReview.status = true;
+        //user 연결
+        return likeReview;
+    }
+
+    public void unLikeReview (Review review) {
+        this.status = false;
+        review.setLikeNum(review.getLikeNum()-1);
+
+    }
+
+
+}
