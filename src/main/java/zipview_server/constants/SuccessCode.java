22cb5af529fe416e54fbf5f692d393f87d345234
Kiltae <loverpleasestayo0@gmail.com>
2023-06-01 18:13:44 +0900

    [FEAT] 리뷰 조회 성공 코드 작성

--- src/main/java/zipview_server/constants/SuccessCode.java
+++ src/main/java/zipview_server/constants/SuccessCode.java
@@ -8,13 +8,16 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum SuccessCode {
-    LOAD_REVIEW_SUCCESS(OK, "리뷰 조회에 성공하셨습니다."),
+    LOAD_REVIEW_SUCCESS(OK, "전체 리뷰 조회에 성공하셨습니다."),
     CREATE_REVIEW_SUCCESS(OK, "리뷰 생성에 성공하셨습니다."),
     REVIEW_DELETE_SUCCESS(OK, "리뷰 삭제에 성공하셨습니다."),
     REVIEW_FIX_SUCCESS(OK, "리뷰 수정에 성공하셨습니다."),
     REVIEW_REPORT_SUCCESS(OK, "리뷰 신고에 성공하셨습니다."),
     REVIEW_LIKE_SUCCESS(OK, "리뷰 좋아요에 성공하셨습니다."),
-    REVIEW_BEST_SUCCESS(OK, "리뷰 조회에 성공하셨습니다."),
+    LOAD_REVIEW_BEST_SUCCESS(OK, "인기있는 리뷰 조회에 성공하셨습니다."),
+    LOAD_REVIEW_HOME_SEARCH_SUCCESS(OK, "홈 화면 리뷰 조회에 성공하셨습니다."),
+    LOAD_REVIEW_FILTER_SUCCESS(OK, "필터링 리뷰 조회에 성공하셨습니다."),
+
 
     LOAD_COMMENT_SUCCESS(OK, "코멘트 조회에 성공하셨습니다."),
     CREATE_COMMENT_SUCCESS(OK, "코멘트 생성에 성공하셨습니다."),
